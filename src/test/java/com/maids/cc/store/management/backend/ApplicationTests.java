014ac5b04c17e62262be795b6a06e469d5b679a0
Ext_Ghadeer.AlHasan <jdoe@example.com>
2021-01-12 15:26:17 +0200

    Init

---
+++ src/test/java/com/maids/cc/store/management/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maids.cc.store.management.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
